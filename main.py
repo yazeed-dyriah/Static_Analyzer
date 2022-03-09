358e3b1fef3f159d47b636f88f423df1ce026c4f
yazeed-dyriah <79354827+yazeed-dyriah@users.noreply.github.com>
2022-03-09 22:08:24 +0200

    update

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 with open("eof.txt") as file:
+
     lines = file.readlines()
 
 
 for line in lines:
     if '==' in line and line.startswith("if") or line.startswith("else if"):
         argument = line[line.index('')]
-
         print("is a magic number or string")
 
-    elif '<' in line or '>' in line or '<=' in line or '>=' in line and line.startswith("if") or line.startswith("else if"):
+        if '==' in line and line.startswith("if") or line.startswith("else if"):
+            print("its a magic string")
 
+    elif '<' in line or '>' in line or '<=' in line or '>=' in line and line.startswith("if") or line.startswith("else if"):
         print("is a magic number because include an 'operation'")
 
     else:
-
         print(" its not a magic number")
