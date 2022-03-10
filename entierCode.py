731de2a0f2490bb1c455dbb0585a4c489b60788c
yazeed-dyriah <79354827+yazeed-dyriah@users.noreply.github.com>
2022-03-10 17:42:42 +0200

    data type

--- entierCode.py
+++ entierCode.py
@@ -131,7 +131,37 @@ def CheckParametersOrder(fileName):
     
 
     
-    
+    ############################################ Check data type
+def do_the_attrbute():
+    result = False
+    with open('eof.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("def"):
+                arguments = line[line.index('(') + 1:line.index(')')]
+
+                arguments = arguments.split(',')
+                if 'int' in arguments[0] \
+                        and 'str' in arguments[1] \
+                        and 'str' in arguments[2]:
+
+                    result = True
+
+    if result == False:
+        print('incorrect arguments order')
+        return
+
+    for line in lines:
+
+        if line.endswith(')'):
+
+            sub_line = line[line.index('(') + 1:line.index(')')].split(',')
+
+            if sub_line[0].isdecimal() and '"' in sub_line[1] and "'" in arguments[2]:
+                pass
+            else:
+                print("incorrect value in call function", line)
+
         
             
             
