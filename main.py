5a43d0920eac58a7a0cf8ac2b2510cff95a14ac9
yazeed-dyriah <79354827+yazeed-dyriah@users.noreply.github.com>
2022-03-10 17:41:11 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-with open("eof.txt") as file:
-
-    lines = file.readlines()
-
-
-for line in lines:
-    if '==' in line and line.startswith("if") or line.startswith("else if"):
-        argument = line[line.index('')]
-        print("is a magic number or string")
-
+def MagicNumber(fileName):
+    with open("eof.txt") as file:
+        lines = file.readlines()
+    for line in lines:
         if '==' in line and line.startswith("if") or line.startswith("else if"):
-            print("its a magic string")
+            argument = line[line.index('')]
+            print("line:")
+            print (line)
+            print("is a magic number or string")
+            print('\n')
+            print ("******************************************************")
 
-    elif '<' in line or '>' in line or '<=' in line or '>=' in line and line.startswith("if") or line.startswith("else if"):
-        print("is a magic number because include an 'operation'")
-
-    else:
-
-        print(" its not a magic number")
+        elif '<' in line or '>' in line or '<=' in line or '>=' in line or '*' in line or '-' in line or '+' in line or '/' in line and line.startswith("if") or line.startswith("else if"):
+            print("line:")
+            print(line)
+            print("is a magic number because include an 'operation'")
+            print('\n')
+            print ("******************************************************")
